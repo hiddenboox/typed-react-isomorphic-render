cbb5ec56b76c4122ffc9db41626703f8d03f04b5
Sebastian Sobociński <hiddenboox@gmail.com>
2016-04-18 12:36:25 +0200

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,44 @@
-function webPageServer(options: {}): void;
+declare module 'react-isomorphic-render' {
+    function webPageServer(options: {}): void;
 
-export = webPageServer;
+    namespace webPageServer {
+        function head(title: any, description: any, meta: any);
+    }
+
+    export = webPageServer;
+}
+
+declare module 'react-isomorphic-render/page-server' {
+
+    function WebPageServer(options: WebPageServer.WebPageServerOptions): void;
+
+    namespace WebPageServer {
+        interface WebPageServerOptions {
+            development: boolean;
+            port: number;
+            web_server: {
+                host: string,
+                port: number
+            };
+            disable_server_side_rendering: boolean;
+            assets(): {};
+            [index: string]: Function|boolean|number|string|{};
+        }
+    }
+
+    export = WebPageServer;
+}
+
+declare module 'react-isomorphic-render/redux' {
+
+    class UniversalRedux {
+        static createStore(reducers: {}, options: {}): { store: {}, reload: Function };
+    }
+
+    namespace UniversalRedux {
+        function render(opt: any): any;
+        function create_store(o: any, p: any): any;
+    }
+
+    export = UniversalRedux;
+}
